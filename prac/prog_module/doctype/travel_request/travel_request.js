ea94b63d76f4c1a4b5b253cbbe00f058afc3f5d1
Pranay <pranay@sanskartechnolab.com>
2025-02-06 17:26:09 +0530

    Initial commit for Frappe app

---
+++ prac/prog_module/doctype/travel_request/travel_request.js
@@ -0,0 +1,26 @@
+// Copyright (c) 2025, Sanskar and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Travel Request", {
+    onload: function(frm) {
+
+        frm.trigger("travel_type");
+        frm.trigger("travel_cost");
+    },
+    travel_type: function(frm) {
+        frm.toggle_display("visa_required", frm.doc.travel_type === "International");
+    },
+    travel_cost: function(frm) {
+        // Show Manager Approval field only if Travel Cost > 1000
+        frm.toggle_display("manager_approval", frm.doc.travel_cost > 1000);
+    },
+    validate: function(frm) {
+        // Ensure logic is checked before submission
+        if (frm.doc.travel_type === "International" && !frm.doc.visa_required) {
+            frappe.throw("Visa is required for international travel.");
+        }
+        if (frm.doc.travel_cost > 1000 && !frm.doc.manager_approval) {
+            frappe.throw("Manager approval is required for travel costs above 1000.");
+        }
+    }
+});
