76d9646f5b68b33cf5531f45d216424202404cda
Pranay <pranay@sanskartechnolab.com>
2025-02-06 18:16:05 +0530

    Initial commit for Frappe app

--- prac/prog_module/doctype/leave_application_doctype/leave_application_doctype.js
+++ prac/prog_module/doctype/leave_application_doctype/leave_application_doctype.js
@@ -1,8 +1,22 @@
 // Copyright (c) 2025, Sanskar and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Leave Application Doctype", {
-// 	refresh(frm) {
+frappe.ui.form.on("Leave Application Doctype", {
 
-// 	},
-// });
+    onload: function(frm) {
+        frm.trigger("leave_type");  // Apply conditions on form load
+        frm.trigger("total_days");
+    },
+
+    leave_type: function(frm) {
+        // Show "Attach Medical Certificate" if Leave Type is "Sick Leave"
+        frm.toggle_display("medical_certificate", frm.doc.leave_type === "Sick");
+
+       
+    },
+
+    total_days: function(frm) {
+        frm.toggle_display("manager_approval", frm.doc.total_days > 3);
+    }
+});
+
