d2d9521d5c3e8f92799d48b70f57b4bd4bbe380c
Pranay <pranay@sanskartechnolab.com>
2025-02-11 18:45:38 +0530

    Adding code after some customsation learning and DocType.

--- prac/prog_module/doctype/employe/employe.py
+++ prac/prog_module/doctype/employe/employe.py
@@ -3,7 +3,10 @@
 
 # import frappe
 from frappe.model.document import Document
+from frappe.model.naming import getseries
 
 
 class Employe(Document):
-	pass
+	def autoname(self):
+		prefix = "P-{}-".format(self.name1)
+		self.name = getseries(prefix, 3)
